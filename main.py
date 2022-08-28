1df4101cfddb27711336bef7c6fbcae23d67cd67
Milton Salustiano <109237764+miltonneto10@users.noreply.github.com>
2022-08-28 08:18:46 -0300

    Commit Inicial

---
+++ main.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from twilio.rest import Client
+account_sid = "AC03b97774a2ba2cb7cbae7dd667007056"
+auth_token  = "ca10aeb34ccbe9f976b396631d7ee25f"
+client = Client(account_sid, auth_token)
+lista_meses = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho']
+
+for mes in lista_meses:
+    tabela_vendas = pd.read_excel(f'{mes}.xlsx')
+    if (tabela_vendas['Vendas'] > 55000).any():
+        vendedor = tabela_vendas.loc[tabela_vendas['Vendas'] > 55000, 'Vendedor'].values[0]
+        vendas = tabela_vendas.loc[tabela_vendas['Vendas'] > 55000, 'Vendas'].values[0]
+        print(f'No mês {mes} alguem bateu a meta. Vendedor: {vendedor}, Vendas: {vendas}')
+        message = client.messages.create(
+            to="+5583981042391",
+            from_="+12724222123",
+            body=f'No mês {mes} alguem bateu a meta. Vendedor: {vendedor}, Vendas: {vendas}')
+        print(message.sid)
